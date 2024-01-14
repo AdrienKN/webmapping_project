90d0d47a51de442fd6097593e81bfb974d3c8c03
ArienKN <adrien.knobloch@gmail.com>
2024-01-14 14:10:15 +0100

    update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,11 @@
+// .vitepress/config.js
 export default {
+    // site-level options
+    title: 'VitePress',
+    description: 'Just playing around.',
+    themeConfig: {
+        // theme-level options
+        },    
     ignoreDeadLinks: true // <-- this will still warn users on dead links but won't throw error
                           // default value is false, i.e. builds will fail on encountering dead links
   }
