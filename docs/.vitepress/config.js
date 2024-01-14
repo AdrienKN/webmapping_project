fb1b15dd370e468f48057ab15ceb55c44038b0b4
ArienKN <adrien.knobloch@gmail.com>
2024-01-14 20:24:51 +0100

    update gitignore

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,28 +1,10 @@
-import { defineConfig } from 'vitepress'
-
-// https://vitepress.dev/reference/site-config
-export default defineConfig({
-  title: "VitePress Website",
-  description: "Website realised by Adrien KNOBLOCH to display my map",
-  ignoreDeadLinks: true,
+// .vitepress/config.js
+export default {
+  // site-level options
+  title: 'VitePress',
+  description: 'Just playing around.',
+  ignoreDeadLinks: true, // <-- this will still warn users on dead links but won't throw error,
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]}
-})
+    // theme-level options
+  }
+}
