5c7d9a6b5c4a01a5b4e4d59a0c9625adcecd0adb
ArienKN <adrien.knobloch@gmail.com>
2024-01-18 16:34:30 +0100

    Add parameters

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -9,30 +9,30 @@ import {
 } from "./chunk-MUVJN64F.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default3 } from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default4 } from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default5 } from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default6 } from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default7 } from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default8 } from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default9 } from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default10 } from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default11 } from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default12 } from "/home/adrien/webmapping_project/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default3 } from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default4 } from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default5 } from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default6 } from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default7 } from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default8 } from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default9 } from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default10 } from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default11 } from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default12 } from "/home/adrien/Documents/SIGMA/902_1_JEGOU/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/vitepress/dist/client/shared.js
 var inBrowser = typeof document !== "undefined";
