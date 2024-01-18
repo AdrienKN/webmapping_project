42e8be246689e44479f79a43d534cde9df73d3b4
AdrienKN <125285400+AdrienKN@users.noreply.github.com>
2024-01-18 09:53:13 +0100

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 // .vitepress/config.js
 export default {
   // site-level options
-  base: '/webmapping_project/docs/',
+  base: '/webmapping_project/',
   title: 'VitePress',
   description: 'Just playing around.',
   ignoreDeadLinks: true, // <-- this will still warn users on dead links but won't throw error,
