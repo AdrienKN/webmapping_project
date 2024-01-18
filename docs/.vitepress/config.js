dd918d1a5248816d593da1b7762af49ce05ea584
AdrienKN <125285400+AdrienKN@users.noreply.github.com>
2024-01-18 09:42:49 +0100

    Correcting spelling mistake

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 // .vitepress/config.js
 export default {
   // site-level options
-  base: '/webapping_project/',
+  base: '/webmapping_project/',
   title: 'VitePress',
   description: 'Just playing around.',
   ignoreDeadLinks: true, // <-- this will still warn users on dead links but won't throw error,
