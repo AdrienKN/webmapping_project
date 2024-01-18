94d0d9adcead8b8176a0d8c3ca4a762267766bb3
ArienKN <adrien.knobloch@gmail.com>
2024-01-18 09:48:17 +0100

    Update 'base' parameter

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 // .vitepress/config.js
 export default {
   // site-level options
-  base: '/webmapping_project/',
+  base: '/webmapping_project/docs/',
   title: 'VitePress',
   description: 'Just playing around.',
   ignoreDeadLinks: true, // <-- this will still warn users on dead links but won't throw error,
